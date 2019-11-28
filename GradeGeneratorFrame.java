ed249f81d495d3c19b0e9fa4dfa872e052db2465
Rajan Elio <r-elio@users.noreply.github.com>
2019-11-28 19:03:02 +0800

    source and setup button added

--- GradeGeneratorFrame.java
+++ GradeGeneratorFrame.java
@@ -1,6 +1,8 @@
 import java.awt.*;
 import java.awt.event.*;
-//import java.beans.*;
+import java.io.*;
+import java.util.*;
+import java.beans.*;
 import javax.swing.*;
 import javax.swing.JSpinner.DefaultEditor;
 
@@ -27,10 +29,15 @@ public class GradeGeneratorFrame extends JFrame {
     private JTextField sourceField;
     private JLabel statusLabel;
     private JTextField statusField;
+    private JButton generateButton;
 
     private static final String[] SUBJECTS = {"MTB","Filipino","English",
                                               "Math","AP", "ESP","Music",
                                               "Arts","PE","Health"};
+
+    private ArrayList<Integer[]> srcGrades;
+    private int[] wrWorkHPS;
+    private int[] perfTaskHPS;
 
     public GradeGeneratorFrame(){
         setTitle("Grade Generator");
@@ -144,6 +151,7 @@ public class GradeGeneratorFrame extends JFrame {
 
         statusLabel = new JLabel("Status:");
         statusField = new JTextField(30);
+        statusField.setText("Please select a source text file and setup the HPS.");
         statusField.setEditable(false);
 
         statusPanel = new JPanel(new GridBagLayout());
@@ -170,6 +178,11 @@ public class GradeGeneratorFrame extends JFrame {
         constraints.anchor = GridBagConstraints.WEST;
         statusPanel.add(statusField, constraints);
 
+        generateButton = new JButton("Generate");
+        generateButton.setFocusable(false);
+
+        generateButton.addActionListener(new GenerateListener());
+
         gradePanel = new JPanel(new GridBagLayout());
         constraints.insets = new Insets(10, 10, 10, 10);
 
@@ -185,8 +198,13 @@ public class GradeGeneratorFrame extends JFrame {
 
         constraints.gridx = 0;
         constraints.gridy = 2;
-        constraints.anchor = GridBagConstraints.SOUTH;
+        constraints.anchor = GridBagConstraints.CENTER;
         gradePanel.add(statusPanel, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 3;
+        constraints.anchor = GridBagConstraints.SOUTH;
+        gradePanel.add(generateButton, constraints);
 
         gradePanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
@@ -208,12 +226,183 @@ public class GradeGeneratorFrame extends JFrame {
 
     private class SourceListener implements ActionListener {
         @Override
-        public void actionPerformed(ActionEvent e) {
-            
+        public void actionPerformed(ActionEvent event) {
+            try {
+                JFileChooser sourceFile = new JFileChooser();
+                sourceFile.showOpenDialog(rootPane);
+
+                FileReader fReader = new FileReader(sourceFile.getSelectedFile());
+                BufferedReader bReader = new BufferedReader(fReader);
+
+                ArrayList<Integer[]> tempGrades = new ArrayList<Integer[]>();
+
+                String line = null;
+                while ((line = bReader.readLine()) != null){
+                    String[] gradeStr = line.split("[ \t]");
+
+                    for (int i = 0; i < gradeStr.length; ++i){
+                        if (gradeStr[i].isEmpty()){
+                            bReader.close();
+                            throw new IOException("Empty String Detected");
+                        }
+                    }
+
+                    Integer[] gradeInt = new Integer[gradeStr.length];
+                    for (int i = 0; i < gradeInt.length; ++i){
+                        gradeInt[i] = Integer.valueOf(gradeStr[i]);
+                    }
+
+                    tempGrades.add(gradeInt);
+                }
+
+                String sourceStr = sourceFile.getSelectedFile().toString();
+                statusField.setText("The Source Text has been retrieved successfully.");
+                sourceField.setText(sourceStr);
+
+                srcGrades = tempGrades;
+
+                bReader.close();
+            }
+            catch (IOException e){
+                JOptionPane.showMessageDialog(rootPane, "IOException:\n" 
+                + e.getMessage(), "I/O Exception", JOptionPane.ERROR_MESSAGE);
+                statusField.setText("The Source Text has not been retrieved successfully.");
+            }
+            catch (NumberFormatException e){
+                JOptionPane.showMessageDialog(rootPane, "NumberFormatException:\n" 
+                + e.getMessage(), "Number Format Exception", JOptionPane.ERROR_MESSAGE);
+                statusField.setText("The Source Text has not been retrieved successfully.");
+            }
+            catch (NullPointerException e){
+                // Expected Exception
+            }
+            catch (Exception e){
+                e.printStackTrace();
+				JOptionPane.showMessageDialog(rootPane, "Unimplemented Exception:\n" 
+				+ e.getMessage(), "Error!", JOptionPane.ERROR_MESSAGE);
+            }
         }
     }
 
     private class SetupListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent event) {
+            setupHPS();
+        }
+    }
+
+    private void setupHPS(){
+        int workItem = (int)wrWorkSpinner.getValue();
+        JLabel[] workLabels = new JLabel[workItem];
+        JTextField[] workFields = new JTextField[workItem];
+
+        int taskItem = (int)perfTaskSpinner.getValue();
+        JLabel[] taskLabels = new JLabel[taskItem];
+        JTextField[] taskFields = new JTextField[taskItem];
+
+        Object[] prompt = new Object[(2 * (workItem + taskItem))];
+        int pIndex = 0;
+
+        Object[] options = {"Save","Cancel"};
+
+        for (int i = 0; i < workItem; ++i){
+            workLabels[i] = new JLabel("Written Work # "+(i+1));
+            prompt[pIndex++] = workLabels[i].getText();
+            workFields[i] = new JTextField();
+            prompt[pIndex++] = workFields[i];
+        }
+
+        for (int i = 0; i < taskItem; ++i){
+            taskLabels[i] = new JLabel("Performance Task # "+(i+1));
+            prompt[pIndex++] = taskLabels[i].getText();
+            taskFields[i] = new JTextField();
+            prompt[pIndex++] = taskFields[i];
+        }
+
+        JOptionPane optionPane = new JOptionPane(prompt, JOptionPane.PLAIN_MESSAGE, 
+        JOptionPane.OK_CANCEL_OPTION, null, options, options[0]);
+
+        String subject = String.valueOf(subjectBox.getSelectedItem());
+
+        JDialog dialog = new JDialog(this, subject, true);
+        dialog.setContentPane(optionPane);
+
+        optionPane.addPropertyChangeListener(new PropertyChangeListener(){
+            @Override
+            public void propertyChange(PropertyChangeEvent event){
+                if (JOptionPane.VALUE_PROPERTY.equals(event.getPropertyName())){
+                    if (optionPane.getValue().equals(options[0])){
+                        optionPane.setValue(JOptionPane.UNINITIALIZED_VALUE);
+                        try {
+                            String[] workStrings = new String[workFields.length];
+                            for (int i = 0; i < workStrings.length; ++i){
+                                workStrings[i] = workFields[i].getText();
+                            }
+
+                            String[] taskStrings = new String[taskFields.length];
+                            for (int i = 0; i < taskStrings.length; ++i){
+                                taskStrings[i] = taskFields[i].getText();
+                            }
+
+                            for (int i = 0; i < workStrings.length; ++i){
+                                if (workStrings[i].isEmpty()){
+                                    JOptionPane.showMessageDialog(rootPane, "You must fill all the required fields.", 
+                                    "Empty Field", JOptionPane.WARNING_MESSAGE);
+                                    return;
+                                }
+                            }
+
+                            for (int i = 0; i < taskStrings.length; ++i){
+                                if (taskStrings[i].isEmpty()){
+                                    JOptionPane.showMessageDialog(rootPane, "You must fill all the required fields.", 
+                                    "Empty Field", JOptionPane.WARNING_MESSAGE);
+                                    return;
+                                }
+                            }
+
+                            int[] workHPS = new int[workStrings.length];
+                            for (int i = 0; i < workHPS.length; ++i){
+                                workHPS[i] = Integer.parseInt(workStrings[i]);
+                            }
+
+                            int[] taskHPS = new int[taskStrings.length];
+                            for (int i = 0; i < taskHPS.length; ++i){
+                                taskHPS[i] = Integer.parseInt(taskStrings[i]);
+                            }
+
+                            wrWorkHPS = workHPS;
+                            perfTaskHPS = taskHPS;
+                            dialog.dispose();
+                        }
+                        catch (NumberFormatException e){
+                            JOptionPane.showMessageDialog(rootPane, "NumberFormatException:\n" 
+                            + e.getMessage(), "Number Format Exception", JOptionPane.ERROR_MESSAGE);
+                        }
+                        catch (Exception e){
+                            e.printStackTrace();
+                            JOptionPane.showMessageDialog(rootPane, "Unimplemented Exception:\n" 
+                            + e.getMessage(), "Error!", JOptionPane.ERROR_MESSAGE);
+                        }
+                        finally {
+                            if (wrWorkHPS != null && perfTaskHPS != null){
+                                statusField.setText("The Grade Generator is ready for use.");
+                            }
+                        }
+                    }
+                    else if (optionPane.getValue().equals(options[1])){
+                        optionPane.setValue(JOptionPane.UNINITIALIZED_VALUE);
+                        dialog.dispose();
+                    }
+                }
+            }
+        });
+
+        dialog.pack();
+        dialog.setLocationRelativeTo(rootPane);
+        dialog.setVisible(true);
+    }
+
+    private class GenerateListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
             
@@ -224,7 +413,7 @@ public class GradeGeneratorFrame extends JFrame {
         GradeGeneratorFrame gradeFrame = new GradeGeneratorFrame();
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-            gradeFrame.setSize(450,275);
+            gradeFrame.setSize(450,300);
             gradeFrame.setResizable(false);
             gradeFrame.setLocationRelativeTo(null);
             gradeFrame.setVisible(true);
